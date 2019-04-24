27337fe610a1ce023a3d5667b1f9619130df7039
Ryan Jamison <ryanjamison26@gmail.com>
2019-04-24 00:38:46 -0400

    hw

---
+++ README.md
@@ -0,0 +1,7 @@
+First I created folders for the Liri App and installed my node packages (exernal & internal) that was needed for the assignment ( node-spotify, axios, fs ). I 
+
+I also created a git ignore folder that housed my api keys and node modules. I created a variable and required the needed files in my liri.js file. 
+
+Once my modules and files were created I created the code for the application. To access the spotify api I used a for loop and dot notation to print the desired information to the terminal. I did the same for the Bands in Town except I used the axios/get method and url to search the api. I also used axios/get method to access the movie information from the Obdm api.
+
+I couldn't get the do-what-this-says command to run properly but when ran in the terminal i didn't get an error message. I attempted to use fs Readfile and create an if statement in the initial spotify1 function to run when that command matched the argument in the terminal. 
--- liri.js
+++ liri.js
@@ -1,6 +1,105 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-
+var axios = require('axios');
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+// var bandsintown = require('bandsintown')('codingbootcamp');
 // Example:
 
 var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+var artist = process.argv[3];
+var queryUrl = axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+
+liriCommand(command, artist);
+
+
+function liriCommand(userCommand, searchTerm){
+    if(userCommand === 'spotify-this'){
+        spotify1(searchTerm)
+        }  if(userCommand === 'spotify-this-song'){
+            read();
+        }
+
+}
+function spotify1(search){
+    spotify.search({ type: 'track', query: search }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       var resultsArray = data.tracks.items;
+       for(i=0; i<resultsArray.length; i++){
+           
+        console.log('Artist:' +  resultsArray[i].album.artists[0].name); 
+        console.log('Album Name:'+ resultsArray[i].album.name); 
+        console.log('Preview Url:' + resultsArray[i].preview_url); 
+        console.log('Track Name:' + resultsArray[i].name); 
+       }
+      
+      
+      });
+}
+
+liriCommandBand(command,artist);
+
+function liriCommandBand(userCommand,searchTerm){
+    if(userCommand === 'concert-this'){
+        band1(searchTerm)
+    }
+}
+function band1(queryUrl){
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response){
+    for(i=0; i<5; i++){
+        console.log('Venue Name:' + '' + response.data[i].venue.name)
+        console.log('Venue Location:' + '' + response.data[i].venue.city + ',' + response.data[i].venue.country)
+        console.log('Venue Date:' + '' + response.data[i].datetime);
+        // console.log('venue:' +response.data[0].venue[0])
+    }    
+    // console.log(response.data[1])
+    });
+}
+
+// var queryUrl = axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+liriCommandMovie(command,artist);
+
+function liriCommandMovie(userCommand, searchTerm){
+    if(userCommand === 'movie-this' ){
+        movie1(searchTerm)
+    };
+}
+  function movie1(queryUrl){
+      title = process.argv.slice(3).join('+')
+    axios.get("http://www.omdbapi.com/?t="+ title + "&y=&plot=short&apikey=trilogy").then(
+        function(response) {
+        //   console.log(response);
+          console.log('Title:'+ response.data.Title);
+          console.log('Year:' + response.data.Year);
+          console.log('Plot:'+ response.data.Plot);
+          console.log('Language:' + response.data.Language);
+          console.log('Actors:'+response.data.Actors);
+          console.log('imdb Rating:' + response.data.imdbRating);
+          console.log('Country:'+response.data.Country);
+          
+        }
+      );
+      
+  }  
+  // Attempted to call read function to run command from random.txt
+  function read(){
+  fs.readFile("random.txt", "utf8", function(error, search) {
+
+    
+    if (error) {
+      return console.log(error);
+    }
+    if(command === 'do=what-it-says'){
+        spotify1(search);
+        console.log(response);
+    }
+       
+    
+  
+
+
+  })
+  }
